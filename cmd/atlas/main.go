d3528ad7c7e9885d4353fea83939146cf1af42b1
Gabriel Cavallo <cavallo.v.gabriel@gmail.com>
2024-08-23 01:48:05 -0300

    Create folder structure

--- cmd/atlas/main.go
+++ cmd/atlas/main.go
@@ -1,13 +1,39 @@
 package main
 
 import (
-	"github.com/terransys/terransys-atlas/internal/eksroles"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"github.com/your-org/terransys-atlas/internal/aws/ekscluster"
+	"github.com/your-org/terransys-atlas/internal/aws/eksinit"
+	"github.com/your-org/terransys-atlas/internal/aws/eksnet"
+	"github.com/your-org/terransys-atlas/internal/aws/eksroles"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		eksroles.CreateRoles(ctx)
+		// Crear IAM roles en AWS
+		roles, err := eksroles.CreateRoles(ctx)
+		if err != nil {
+			return err
+		}
+
+		// Configurar la red en AWS
+		network, err := eksnet.SetupNetwork(ctx)
+		if err != nil {
+			return err
+		}
+
+		// Desplegar el clúster EKS en AWS
+		cluster, err := ekscluster.DeployCluster(ctx, roles, network)
+		if err != nil {
+			return err
+		}
+
+		// Inicializar el clúster con configuraciones necesarias en AWS
+		err = eksinit.InitializeCluster(ctx, cluster)
+		if err != nil {
+			return err
+		}
+
 		return nil
 	})
 }
